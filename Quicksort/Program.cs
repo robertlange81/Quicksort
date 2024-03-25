921640fbc552f44a38cfc90c8654e14e65b4cdca
Robert Lange <robert.lange.81@gmail.com>
2024-03-25 21:47:43 +0100

    init

---
+++ Quicksort/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Quicksort");
+// Random rnd = new Random();
+
+int[] unsortierteListe =  new int[] { 9, 2, 3, 8, 5, 4, 11, 22, 24, 22, 23, 0, -1 };
+
+
+Quicksort.Quicksort.quicksort(unsortierteListe, 0, unsortierteListe.Length - 1);
+Console.ReadKey();
---
+++ Quicksort/Quicksort.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quicksort
+{
+    class Quicksort
+    {
+        public static void quicksort(int[] liste, int links, int rechts)
+        {
+            if (links < rechts)
+            {
+                foreach (int i in liste)
+                {
+                    Console.Write(i + " ");
+                }
+                Console.WriteLine();
+                int teiler = teile(liste, links, rechts);
+                quicksort(liste, links, teiler - 1);
+                quicksort(liste, teiler + 1, rechts);
+            }
+        }
+
+        public static int teile(int[] liste, int links, int rechts)
+        {
+            int i = links;
+            int j = rechts - 1;
+            int pivot = liste[rechts];
+
+
+            while (i < j)
+            {
+                for (; i < j && liste[i] <= pivot; i++)
+                {
+                }
+
+                for (; i < j && liste[j] > pivot; j--)
+                {
+                }
+
+                if (liste[i] > liste[j])
+                {
+                    int tmp = liste[i];
+                    liste[i] = liste[j];
+                    liste[j] = tmp;
+                }
+            }
+            if (liste[i] > pivot)
+            {
+                liste[rechts] = liste[i];
+                liste[i] = pivot;
+            } else {
+                i = rechts;
+            }
+            return i;
+        }
+    }
+}
